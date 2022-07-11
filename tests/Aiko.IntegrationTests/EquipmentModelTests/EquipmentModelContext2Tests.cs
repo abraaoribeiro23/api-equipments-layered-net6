a195baa71424229dbf8268d3baee35c8555b193a
abraaoribeiro23 <abraaoribeiroo23@gmail.com>
2022-07-10 21:47:35 -0300

    test: setup of integration tests with data injection

---
+++ tests/Aiko.IntegrationTests/EquipmentModelTests/EquipmentModelContext2Tests.cs
@@ -0,0 +1,27 @@
+﻿using Aiko.Infra.Repositories;
+using Aiko.UnitTests.DataInjectors;
+using Xunit;
+
+namespace Aiko.IntegrationTests.EquipmentModelTests;
+
+public class EquipmentModelContext2Tests : IClassFixture<ServiceProviderFixture>
+{
+    private readonly ServiceProviderFixture _fixture;
+
+    public EquipmentModelContext2Tests(ServiceProviderFixture fixture)
+    {
+        _fixture = fixture;
+        InjectDataOnContext.InitializeDbForTests(fixture.SqlContextFixture);
+    }
+
+    [Fact]
+    public void EquipmentModelContext_Exist()
+    {
+        var repopository = new EquipmentModelRepository(_fixture.SqlContextFixture);
+        var result = repopository.GetAll();
+        Assert.NotNull(result);
+        var equipmentModels = result.ToList();
+        Assert.NotEmpty(equipmentModels);
+        Assert.Equal(3, equipmentModels.Count);
+    }
+}
---
+++ tests/Aiko.IntegrationTests/EquipmentModelTests/EquipmentModelContextTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using Aiko.Domain.Models;
+using Aiko.Infra.Repositories;
+using Aiko.UnitTests.DataInjectors;
+using Xunit;
+
+namespace Aiko.IntegrationTests.EquipmentModelTests;
+
+public class EquipmentModelContextTests : IClassFixture<ServiceProviderFixture>
+{
+    private static readonly Type ClassType = typeof(EquipmentModelContextTests);
+    private static readonly Assembly? Assembly = Assembly.GetAssembly(ClassType);
+    private static readonly string JsonPath = $"{ClassType.Namespace}.SeedData";
+
+    private readonly ServiceProviderFixture _fixture;
+
+    public EquipmentModelContextTests(ServiceProviderFixture fixture)
+    {
+        _fixture = fixture;
+        var equipmentModelJson =
+            Assembly?.GetManifestResourceStream($"{JsonPath}.equipment_model_001.json");
+        InjectDataOnContext.AddInContext<EquipmentModel>(fixture.SqlContextFixture, equipmentModelJson);
+    }
+
+    [Fact]
+    public void EquipmentModelContext_Exist()
+    {
+        var repopository = new EquipmentModelRepository(_fixture.SqlContextFixture);
+        var result = repopository.GetAll();
+        Assert.NotNull(result);
+        var equipmentModels = result.ToList();
+        Assert.NotEmpty(equipmentModels);
+        Assert.Single(equipmentModels);
+    }
+}
